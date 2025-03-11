d3ea9be29885f23b12429c738609524f5bdd7606
Balazs Fentor <fentorb24@gmail.com>
2025-03-11 20:00:46 +0200

    We're so done

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Num1;
+    private EditText Num2;
+    private TextView OutputValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Num1 = findViewById(R.id.FirstNumber);
+        Num2 = findViewById(R.id.SecondNumber);
+
+        OutputValue = findViewById(R.id.Output);
+    }
+
+    public void add(View view) {
+        try {
+            int result = Integer.parseInt(Num1.getText().toString()) + Integer.parseInt(Num2.getText().toString());
+            OutputValue.setText(Integer.toString(result));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+    public void subtract(View view) {
+        try {
+            int result = Integer.parseInt(Num1.getText().toString()) - Integer.parseInt(Num2.getText().toString());
+            OutputValue.setText(Integer.toString(result));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+    public void multiply(View view) {
+        try {
+            int result = Integer.parseInt(Num1.getText().toString()) * Integer.parseInt(Num2.getText().toString());
+            OutputValue.setText(Integer.toString(result));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+    public void divide(View view) {
+        try {
+            int result = Integer.parseInt(Num1.getText().toString()) / Integer.parseInt(Num2.getText().toString());
+            OutputValue.setText(Integer.toString(result));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
     }
 }
